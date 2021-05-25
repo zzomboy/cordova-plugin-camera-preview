b96f7a9632f20da5938396846bb736228eb985fb
Karanpoj Varintaravet <31284177+zzomboy@users.noreply.github.com>
2021-05-25 12:15:12 +0700

    Update edit_manifest.js

--- hooks/android/edit_manifest.js
+++ hooks/android/edit_manifest.js
@@ -75,7 +75,7 @@ var disableAllowBackup = (function () {
 })();
 
 module.exports = function (ctx) {
-    var Q = ctx.requireCordovaModule("q");
+    var Q = ctx.require("q");
     fs = ctx.requireCordovaModule("fs");
     path = ctx.requireCordovaModule("path");
     elementtree = ctx.requireCordovaModule("elementtree");
